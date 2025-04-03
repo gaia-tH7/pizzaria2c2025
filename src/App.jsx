716cec9fbfa125d898c947b44da76a5c008f65de
gaia-tH7 <rm94318@estudante.fieb.edu.br>
2025-04-03 08:26:31 -0300

    envio de parametros

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,34 @@ function App(){
 
   const [nome, setNome]= useState("Ronaldo")
 
-  const Formulario = () => {
+  const Formulario = (props) => {
 
     return (
       <div>
         <input
+        placeholder={props.sombra}
       className="nome"
       onChange={(e)=>{setNome(e.target.value)}}
       name="nome"
       type="text" />
       <button
-      onClick={()=>{alert(nome)}}
+      onClick={()=>{(props.mensagem != null) ? alert(props.mensagme)
+                     : alert("TEXTO PADRÃO") }}
       className="butao">
-        CLIQUE AQUI
+        {(props.textoBotao != null)? props.textoBotao : "CLIQUE AQUI"}
       </button>
       </div>
     )
   }
   return (
+
     <div>
       <h3>Pizzaria 2C</h3>
-      <Formulario />
-      <Formulario />
-      <Formulario />
+      <Formulario  sombra="Digite seu nome..." textoBotao= "ENVIAR NOME" />
+      <Formulario  sombra="Digite seu email..." textoBotao="E-MAIL" />
+      <Formulario  sombra="(11) 98000-0000" textoBotao="CELULAR" />
+      <Formulario  mensagem="Mensagem enviada pelo App" />
+
     </div>
   );
 }
